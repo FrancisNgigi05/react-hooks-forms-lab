23738e040b80439692381f748fc99c8afa17cdd9
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-06-07 12:51:21 +0300

    Done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,29 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [filterText, setFilterText] = useState("");
+  const [goods, setGoods] = useState(items);
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  const onItemFormSubmit = (obj) => {
+    setGoods([...goods,obj]);
+  }
+  const itemsToDisplay = goods.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
-  });
+  })
+  .filter((item)=>item.name.toLowerCase().includes(filterText.toLocaleLowerCase()));
+
+
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={setFilterText} search={filterText}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
